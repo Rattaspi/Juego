4df51e154e6c1fc279739a8ccb1df3fadcb8cbf3
Devildrake <divadarrei@gmail.com>
2018-10-26 11:42:39 +0200

    LOG: - Creadas las carpetas Resources y Scripts - TODOS LOS MATERIALES Y TEXTURAS DEBEN IR DENTRO DE RESOURCES EN SU CARPETA CORRESPONDIENTE PARA  EVITAR SOBREPOBLACIÓN DE CARPETAS - Establecida una escena con el juego de la selección de cartas, hay una disposición de 12 cartas en 2 filas, por ahora no es algo que se cambie con facilidad - Los animales y colores de las cartas son aleatorios, pero el color aleatorio debería estar más diferenciado (WIP)

---
+++ Animal_Shelter/Assets/Scripts/CardMiniGame_Card.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMiniGame_Card : MonoBehaviour {
+    public int id;
+    public CardMiniGame_Card sister;
+    public int cardValue;
+    public bool highlighted;
+    public bool clicked;
+    public bool wasClicked;
+    public bool rotated;
+    float highlightTimer=0;
+    Vector3 originalScale;
+    Vector3 highlightedScale;
+    Quaternion originalRotation;
+    float originalRotationAngle;
+    Vector3 originalRotationiVector;
+    float destinedRotation = 180.0f;
+    public float sigma,sigma2;
+    Vector3 tempScale;
+
+    // Use this for initialization
+    void Start () {
+        originalRotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+        transform.rotation.ToAngleAxis(out originalRotationAngle, out originalRotationiVector);
+
+        originalScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        highlightedScale=new Vector3(originalScale.x+originalScale.x*0.1f , originalScale.y + originalScale.y * 0.1f , originalScale.z + originalScale.z * 0.1f);
+        tempScale = new Vector3(0, 0, 0);
+        sigma = 0;
+        highlighted = clicked = false;
+        rotated = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!clicked) {
+            if (wasClicked) {
+                UnClickBehavior();
+            }
+            HighlightManagement();
+            HighlightLerp();
+        } else {
+            ClickedBehavior();
+        }
+    }
+
+    private void OnMouseOver() {
+        highlighted = true;
+        highlightTimer = 0;
+    }
+
+
+
+    void UnClickBehavior() {
+        if (sigma2 > 0) {
+            float tempAngle;
+            Vector3 rotationVector;
+            transform.rotation.ToAngleAxis(out tempAngle, out rotationVector);
+            transform.rotation = Quaternion.AngleAxis(Mathf.Lerp(originalRotationAngle, tempAngle, sigma2), new Vector3(0, 1, 0));
+            sigma2 -= Time.deltaTime;
+        } else {
+            sigma2 = 0;
+            wasClicked = false;
+        }
+    }
+
+    void ClickedBehavior() {
+        wasClicked = true;
+        if (sigma2 < 1) {
+            float tempAngle;
+            Vector3 rotationVector;
+            transform.rotation.ToAngleAxis(out tempAngle, out rotationVector);
+            transform.rotation = Quaternion.AngleAxis(Mathf.Lerp(tempAngle, destinedRotation, sigma2), new Vector3(0, 1, 0));
+            sigma2 += Time.deltaTime;
+        } else {
+            sigma2 = 1;
+            rotated = true;
+        }
+    }
+
+    private void OnMouseEnter() {
+        if (CardMiniGame_Logic.cardLogic != null) {
+            CardMiniGame_Logic.cardLogic.currentCard = this;
+        }
+    }
+
+    private void OnMouseExit() {
+        if (CardMiniGame_Logic.cardLogic != null) {
+            if (CardMiniGame_Logic.cardLogic.currentCard == this) {
+                CardMiniGame_Logic.cardLogic.currentCard = null;
+            }
+        }
+    }
+
+    void HighlightManagement() {
+        if (highlighted) {
+            highlightTimer += Time.deltaTime;
+            if (sigma < 1) {
+                sigma += Time.deltaTime * 3;
+            } else {
+                sigma = 1;
+            }
+        } else {
+            if (sigma > 0) {
+                sigma -= Time.deltaTime * 3;
+            } else {
+                sigma = 0;
+            }
+        }
+        if (highlightTimer > 0.15f) {
+            highlighted = false;
+        }
+    }
+
+    void HighlightLerp() {
+        tempScale.x = Mathf.Lerp(originalScale.x, highlightedScale.x, sigma);
+        tempScale.y = Mathf.Lerp(originalScale.y, highlightedScale.y, sigma);
+        tempScale.z = Mathf.Lerp(originalScale.z, highlightedScale.z, sigma);
+        transform.localScale = tempScale;
+    }
+
+}
---
+++ Animal_Shelter/Assets/Scripts/CardMiniGame_Logic.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMiniGame_Logic : MonoBehaviour {
+    [HideInInspector]
+    public static CardMiniGame_Logic cardLogic;
+    public CardMiniGame_Card currentCard;
+    public GameObject cardPrefab;
+    CardMiniGame_Card card1, card2;
+    public CardMiniGame_Card[] cards;
+    public int score;
+    public int tries;
+    public int numOfCards;
+    public int cardsPerRow;
+    Material backMaterial;
+    // Use this for initialization
+
+    private void Awake() {
+        if (cardLogic == null) {
+            cardLogic = this;
+        } else {
+            Destroy(this);
+        }
+    }
+
+    void FindFreeId(CardMiniGame_Card card) {
+        bool found = false;
+        int controlInt = 0;
+        while (!found&&controlInt<200) {
+            int randomNumber = Random.Range(0, numOfCards);
+            if (cards[randomNumber].sister == null&&card!=cards[randomNumber]) {
+                found = true;
+                cards[randomNumber].sister = card;
+                card.sister = cards[randomNumber];
+            }
+            controlInt++;
+        }
+    }
+
+
+    void SetUpCards() {
+        cards = new CardMiniGame_Card[numOfCards];
+
+        for(int i = 0; i < numOfCards; i++) {
+            if (i < cardsPerRow) {
+                Vector3 tempPos = new Vector3(-7.5f+i*3,2f,-1);
+                cards[i] = Instantiate(cardPrefab, tempPos,Quaternion.identity).GetComponent<CardMiniGame_Card>();
+            } else {
+                Vector3 tempPos = new Vector3(-7.5f + i%6 * 3, -2.5f, -1);
+                cards[i] = Instantiate(cardPrefab, tempPos, Quaternion.identity).GetComponent<CardMiniGame_Card>();
+            }
+            cards[i].gameObject.GetComponent<MeshRenderer>().material = backMaterial;
+        }
+
+        for (int i = 0; i < numOfCards; i++) {
+            if (cards[i].GetComponent<CardMiniGame_Card>().sister == null) {
+                FindFreeId(cards[i]);
+                //Color tempColor;
+                
+                Material tempMaterial = Resources.Load<Material>("Materials/CardBackMaterial");
+
+                int randomMaterial = Random.Range(0, 4);
+
+                float randomColorR = Random.Range(50, 250);
+                float randomColorG = Random.Range(50, 250);
+                float randomColorB = Random.Range(50, 250);
+
+                randomColorB = randomColorB/255;
+                randomColorR = randomColorR/255;
+                randomColorG = randomColorG/255;
+
+                switch (randomMaterial) {
+                    case 0:
+                        tempMaterial = new Material(Resources.Load<Material>("Materials/BearCardMaterial"));
+                        tempMaterial.color = new Color(randomColorR, randomColorG, randomColorB);
+                        break;
+                    case 1:
+                        tempMaterial = new Material(Resources.Load<Material>("Materials/DeerCardMaterial"));
+                        tempMaterial.color = new Color(randomColorR, randomColorG, randomColorB);
+
+                        break;
+                    case 2:
+                        tempMaterial = new Material(Resources.Load<Material>("Materials/ElephantCardMaterial"));
+                        tempMaterial.color = new Color(randomColorR, randomColorG, randomColorB);
+
+                        break;
+                    case 3:
+                        tempMaterial = new Material(Resources.Load<Material>("Materials/GiraffeCardMaterial"));
+                        tempMaterial.color = new Color(randomColorR, randomColorG, randomColorB);
+
+                        break;
+                    case 4:
+                        tempMaterial = new Material(Resources.Load<Material>("Materials/PandaCardMaterial"));
+                        tempMaterial.color = new Color(randomColorR, randomColorG, randomColorB);
+
+                        break;
+                }
+
+                //cards[i].gameObject.GetComponentInChildren<MeshRenderer>().material = cards[i].sister.gameObject.GetComponentInChildren<MeshRenderer>().material = tempMaterial;
+                //Debug.Log(cards[i].gameObject.GetComponentInChildren<EmptyScript>());
+
+
+                cards[i].gameObject.GetComponentInChildren<EmptyScript>().gameObject.GetComponent<MeshRenderer>().material = tempMaterial;
+                cards[i].sister.gameObject.GetComponentInChildren<EmptyScript>().gameObject.GetComponent<MeshRenderer>().material = tempMaterial;
+
+                //cards[i].gameObject.GetComponentInChildren<EmptyScript>().gameObject.GetComponent<MeshRenderer>().material.color = new Color(randomColorR, randomColorG, randomColorB);
+                //cards[i].sister.gameObject.GetComponentInChildren<EmptyScript>().gameObject.GetComponent<MeshRenderer>().material.color = new Color(randomColorR, randomColorG, randomColorB);
+
+
+            }
+        }
+
+    }
+
+        void Start () {
+        backMaterial = Resources.Load<Material>("Materials/CardBackMaterial");
+        score = tries = 0;
+        numOfCards = 12;
+        cardsPerRow = 6;
+        cardPrefab = Resources.Load<GameObject>("Prefabs/CardPrefab");
+        SetUpCards();
+
+	}
+
+    void CheckCards() {
+        if (card1.sister == card2) {
+            score += card1.cardValue;
+        } else {
+            card1.clicked = card1.highlighted =  false;
+            card2.clicked = card1.highlighted = false;
+        }
+        card1 = card2 = null;
+        tries++;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (card2 != null) {
+            if (card2.rotated) {
+                CheckCards();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (card1 == null || card2 == null) {
+                if (currentCard != null) {
+                    if (!currentCard.clicked) {
+                        currentCard.clicked = true;
+                        currentCard.rotated = false;
+                        currentCard.sigma2 = 0;
+                        if (card1 == null) {
+                            card1 = currentCard;
+                        } else if (currentCard != card1) {
+                            card2 = currentCard;
+                        }
+                    }
+                }
+            }
+        }
+	}
+}
