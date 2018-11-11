6e3f3b42fc6195668f67357f56f4a6c603640c7f
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2018-11-12 00:24:06 +0100

    Wheen you place the pointer over an animal apears a rectangle with the info preview.

--- Animal_Shelter/Assets/Scripts/Animals/PreviewInfoAnimal.cs
+++ Animal_Shelter/Assets/Scripts/Animals/PreviewInfoAnimal.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PreviewInfoAnimal : MonoBehaviour {
+    Animal animalInfo;
+    [SerializeField] Text displayName;
+    [SerializeField] Image happiness;
+    [SerializeField] Image injured;
+    public Sprite[] faces;
 
-	void Start () {
-		
-	}
-	
-	void Update () {
+    void Awake () {
+        animalInfo = GetComponentInParent<Animal>();
+        if (animalInfo == null) Debug.LogError("Animal script not found");
+
+        //Get the faces sprites
+        faces = new Sprite[3];
+        for(int i = 0; i<3; i++) {
+            faces[i] = Resources.Load<Sprite>("Sprites/AnimalPreviewInfo/cara_" + i);
+        }
+
+        displayName.text = animalInfo.nombre;
+        happiness.sprite = faces[(int)animalInfo.confort];
+        if (animalInfo.estado != Animal.ESTADO.SALUDABLE) injured.gameObject.SetActive(true);
+        else injured.gameObject.SetActive(false);
+    }
+
+    void Update () {
 		
 	}
 }
--- Animal_Shelter/Assets/Scripts/Animals/ShowInfoAnimal.cs
+++ Animal_Shelter/Assets/Scripts/Animals/ShowInfoAnimal.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowInfoAnimal : MonoBehaviour {
+    Animal animalInfo;
 
-	void Start () {
-		
+	void Awake () {
+        animalInfo = GetComponentInParent<Animal>();
+        if (animalInfo == null) Debug.LogError("Animal script not found");
 	}
 	
 	void Update () {
