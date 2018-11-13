fc1122b7d0710b003d2b28824d17d02a558771a3
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2018-11-13 13:48:30 +0100

    Avatar movement.

---
+++ Animal_Shelter/Assets/Scripts/MinijuegoTempleRun/TempleRunAvatar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempleRunAvatar : MonoBehaviour {
+    [SerializeField] Transform[] positions;
+    int currentPos;
+
+	void Start () {
+        currentPos = 1;
+        this.transform.position = positions[currentPos].position;
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            currentPos--;
+            if (currentPos < 0) currentPos = 2;
+            this.transform.position = positions[currentPos].position;
+            print(currentPos);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            currentPos = ++currentPos % 3;
+            this.transform.position = positions[currentPos].position;
+        }
+    }
+}
