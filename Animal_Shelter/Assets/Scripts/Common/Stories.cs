6e8599cbf865054917b9f5a0b15b976e4d1f8e66
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2018-11-29 23:41:05 +0100

    Added the stories to a new static class called Stories.

---
+++ Animal_Shelter/Assets/Scripts/Common/Stories.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Stories {
+    public static string[] inicio = {
+        "Era la estrella de un circo",
+        "Huyó",
+        "Sus dueños lo abandonaron y no tuvo más remedio que vagar por las calles",
+        "Vivía junto a sus hermanos en un tonel de manzanas en un callejón",
+        "Estaba buscando comida para su familia cuando un coche le dio un golpe. Un hombre lo llevó al hospital veterinario",
+        "Llegó el verano y sus amos no podían mantenerlo, lo dejaron en una gasolinera",
+        "Una señora mayor vivía rodeada de muchos como él, pero un día comenzó a comérselos para no morir de hambre hasta que la internaron en el manicomio"
+    };
+
+    public static string[] nudo = {
+        " de Las Vegas. Un ludópata lo adoptó y acabó apostándolo en una de sus partidas. Siguió pasando de mano en mano",
+        " de Chernóbil. Al ser víctima de la radiación, nadie quería hacerse cargo de él debido a sus deformidades",
+        " de Pekín. Un cocinero lo adoptó hasta que un día se quedó sin carne y necesitaba usarlo a él para satisfacer a sus clientes. Estaba encerrado en un cuarto oscuro",
+        " de Tokio. Una colegiala lo encontró de camino a casa y lo convirtió en la mascota de su grupo de baile",
+        " de Palestina. Debido a la guerra tuvo que seguir huyendo por mucho tiempo hasta encontrar un lugar mejor",
+        " de la India. Rápidamente se unió a un grupo de monos callejeros que robaban a turistas",
+        " de Barcelona. Una pareja de ancianos lo adoptó, pero meses más tarde murieron. Unos amigos de estos lo acogieron, pero también murieron. A partir de entonces lo repudiaron por ser un mensajero de la muerte",
+        " de Paris. Rompió a llorar sin consuelo hasta que una niña le dio un trozo de su croissant para calmarlo. La persiguió"
+    };
+
+    public static string[] desenlace = {
+        " hasta que tropezó con un militante de Green Peace, que lo trajo hasta aquí.",
+        " hasta que una familia de magos lo incorporó a su espectáculo. Entonces sufrió una lesión y dejó de ser útil.",
+        " hasta que una anciana vendedora de flores lo confundió con su antigua mascota, ya fallecida, y lo intentó enterrar vivo en el sitio dónde se supone que debía estar.",
+        " hasta que llegó la policía y los detuvo a todos. Entonces lo enviaron aquí.",
+        " hasta que un enorme animal comenzó a perseguirlo, llevándolo a la puerta del refugio.",
+        " hasta que anocheció y llegó aquí sin saber cómo.",
+        " hasta que una infestación de garrapatas le hizo rodar para rascarse y cayó cuesta abajo llegando aquí.",
+        " hasta que se dio cuenta que no debía seguir allí y trató de volver a sus orígenes. Con bastante poco éxito.",
+        " hasta que sufrió un infarto y no supieron que más hacer con él."
+    };
+}
