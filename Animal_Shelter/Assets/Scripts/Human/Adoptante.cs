9753ad3aae0b3ee662182ae3dc5b700633c079f0
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2018-12-05 00:00:28 +0100

    The adopter is created by adding the Adoptante.cs script to an empty gameobject. The adopter bubble is created and a preferred animal size is set. An human sprite is chosen randomly.

---
+++ Animal_Shelter/Assets/Scripts/Human/Adoptante.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adoptante : MonoBehaviour {
+    Canvas canvas;
+    [Header("Adopter preferences")]
+    [SerializeField] Animal.SIZE sizePreferred;
+
+	void Start () {
+        canvas = GetComponentInParent<Canvas>();
+        this.transform.position = new Vector2(Screen.width / 2, Screen.height / 2);
+
+        GameObject g = Resources.Load<GameObject>("Prefabs/Humans/HumanGraphics");
+        g = Instantiate(g, this.transform);
+        g.name = "Graphics";
+
+        //Initialize adopter preferences
+        sizePreferred = (Animal.SIZE)Random.Range(0, (int)Animal.SIZE.LENGTH);
+	}
+}
---
+++ Animal_Shelter/Assets/Scripts/Human/GraphicsAdoptante.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicsAdoptante : MonoBehaviour {
+
+	void Start () {
+        GameObject humanSprite = new GameObject();
+        humanSprite.name = "PjImage";
+        humanSprite.transform.parent = this.transform;
+        humanSprite.transform.localPosition = Vector2.zero;
+
+        Image i = humanSprite.AddComponent<Image>();
+        i.sprite = Resources.Load<Sprite>("Sprites/Humans/pj_" + Random.Range(1, 30));
+
+        RectTransform rectTransform = humanSprite.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(250, 250);
+	}
+}
