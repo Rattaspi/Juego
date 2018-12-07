ae7fa6da7ff99f1c0f04cb452ccd4177f79eecd2
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2018-12-07 15:27:43 +0100

    The 60% of the times the adopters want an specie which you have in the shelter

--- Animal_Shelter/Assets/Scripts/Human/Adoptante.cs
+++ Animal_Shelter/Assets/Scripts/Human/Adoptante.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Adoptante : MonoBehaviour {
     Canvas canvas;
+    GameLogic gamelogic;
     [Header("Adopter preferences")]
     [SerializeField] Animal.SIZE sizePreferred;
+    [SerializeField] Animal.ESPECIE speciePreferred;
 
-	void Start () {
+    private void Awake() {
+        gamelogic = FindObjectOfType<GameLogic>();
+    }
+
+    void Start () {
         canvas = GetComponentInParent<Canvas>();
         this.transform.position = new Vector2(Screen.width / 2, Screen.height / 2);
 
@@ -17,5 +23,18 @@ public class Adoptante : MonoBehaviour {
 
         //Initialize adopter preferences
         sizePreferred = (Animal.SIZE)Random.Range(0, (int)Animal.SIZE.LENGTH);
-	}
+        if(gamelogic.shelterAnimals.Count > 0) {
+            float rng = Random.Range(0.0f, 1.0f);
+            if(rng < 0.6f) {
+                int id = Random.Range(0, gamelogic.shelterAnimals.Count);
+                speciePreferred = gamelogic.shelterAnimals[id].especie;
+            }
+            else {
+                speciePreferred = (Animal.ESPECIE)Random.Range(0, (int) Animal.ESPECIE.LENGTH);
+            }
+        }
+        else {
+            speciePreferred = (Animal.ESPECIE)Random.Range(0, (int)Animal.ESPECIE.LENGTH);
+        }
+    }
 }
