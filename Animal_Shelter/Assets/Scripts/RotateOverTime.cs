029bc17ce2e4ca0e13730c45aed9e0c5fcdc620e
Devildrake <divadarrei@gmail.com>
2018-12-12 21:36:21 +0100

    Establecido gráfico del reloj de arena que ya gira.

---
+++ Animal_Shelter/Assets/Scripts/RotateOverTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour {
+    public float periodTime;
+    float timer;
+    //public float delta;
+    Quaternion currentRotation;
+    // Use this for initialization
+    void Start () {
+        periodTime = 1 / periodTime;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float delta = timer / periodTime;
+        timer += GameTime.deltaTime;
+        if (timer >= periodTime) {
+            timer -= periodTime;
+        }
+        currentRotation = Quaternion.AngleAxis(delta * 360, new Vector3(0, 0, 1));
+        transform.rotation = currentRotation;
+	}
+}
