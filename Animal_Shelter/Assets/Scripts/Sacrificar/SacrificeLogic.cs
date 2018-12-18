4ad97eb045b695622287754b403fdebc8e18ac8c
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2018-12-18 14:31:56 +0100

    The sacrifice screen is working.

--- Animal_Shelter/Assets/Scripts/Sacrificar/SacrificeLogic.cs
+++ Animal_Shelter/Assets/Scripts/Sacrificar/SacrificeLogic.cs
@@ -1,28 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SacrificeLogic : MonoBehaviour {
     [SerializeField] Transform animalPosition;
+    [SerializeField] FaderScript fader;
+    [SerializeField] GameObject mouseSyringe;
+    [SerializeField] GraphicRaycaster graphicRaycaster;
+    GameObject animalDisplayed;
 
-    private void Start() {
-        GameObject animal = new GameObject();
-        animal.transform.parent = animalPosition;
-        animal.transform.localPosition = Vector2.zero;
-        //Animal a = animal.AddComponent<Animal>();
+    private void Awake() {
+        graphicRaycaster = GetComponentInParent<GraphicRaycaster>();
+    }
+
+    private void OnEnable() {
         Animal animalSacr = GameLogic.instance.animalToSacrifice;
-        print(animalSacr);
-        //print(aCopy);
-        //a.StartStats(aCopy.size, aCopy.edad, aCopy.confort, aCopy.estado, aCopy.especie, aCopy.hambriento, "", aCopy.color, "", aCopy.salud, aCopy.confortValue, aCopy.hambre);
-        //animal.GetComponent<AnimalMovement>().enabled = false;
-        GameObject g = Instantiate(GameLogic.instance.animalGraphics[(int)animalSacr.especie], animalPosition);
-        TintAnimalPart[] t = g.GetComponentsInChildren<TintAnimalPart>();
+        GameObject animalDisplayed = Instantiate(GameLogic.instance.animalGraphics[(int)animalSacr.especie], animalPosition);
+        TintAnimalPart[] t = animalDisplayed.GetComponentsInChildren<TintAnimalPart>();
         foreach(TintAnimalPart i in t){
             i.ForcePaint(animalSacr.color);
         }
+
+        StartCoroutine(fader.UnFade());
     }
 
     public void SacrificeClick() {
+        print(mouseSyringe.activeInHierarchy);
+        if (mouseSyringe.activeInHierarchy) {
+            StartCoroutine(Sacrifice());
+        }
+    }
 
+    IEnumerator Sacrifice() {
+        graphicRaycaster.enabled = false;
+        CanvasGroup group = animalPosition.gameObject.AddComponent<CanvasGroup>();
+        float animalFadeSpeed = 1.0f;
+        while(group.alpha > 0.0f) {
+            group.alpha -= animalFadeSpeed * Time.deltaTime;
+            yield return null;
+        }
+        group.alpha = 0.0f;
+        StartCoroutine(fader.Fade());
+        while (fader.doing) {
+            yield return null;
+        }
+        Destroy(animalDisplayed);
+        GameLogic.instance.RemoveAnimal(GameLogic.instance.animalToSacrifice);
+        GameLogic.instance.animalToSacrifice = null;
+        graphicRaycaster.enabled = true;
+
+        this.gameObject.SetActive(false);
     }
 }
