f3a71a379d2f40a2112acf1c2a688a9e4e771a34
Alex Canut/ASUS-DEV <a.canutperdigon@gmail.com>
2018-12-27 18:48:08 +0100

    Added win/lose screen + credits sceen. There are two scenes named "Win" and "Lose". Load those depending the ending.

---
+++ Animal_Shelter/Assets/Scripts/FinalSceens/Credits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+    float timeToMainMenu = 10.0f;
+
+	void Start () {
+        FaderScript.instance.unFade = true;
+        StartCoroutine(GoToMainMenu());
+	}
+
+    IEnumerator GoToMainMenu() {
+        AsyncOperation op = SceneManager.LoadSceneAsync("MainMenu");
+        op.allowSceneActivation = false;
+        yield return new WaitForSeconds(timeToMainMenu);
+        FaderScript.instance.fade = true;
+        yield return new WaitForSeconds(1.0f);
+        op.allowSceneActivation = true;
+    }
+}
---
+++ Animal_Shelter/Assets/Scripts/FinalSceens/WinLoseSceen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoseSceen : MonoBehaviour {
+    [SerializeField] float timeUntilCredits = 5.0f;
+
+	void Start () {
+        FaderScript.instance.unFade = true;
+        StartCoroutine(LoadSceneAndWait());
+	}
+
+    IEnumerator LoadSceneAndWait() {
+        AsyncOperation op = SceneManager.LoadSceneAsync("Credits");
+        op.allowSceneActivation = false;
+
+        yield return new WaitForSeconds(timeUntilCredits);
+        FaderScript.instance.fade = true;
+        yield return new WaitForSeconds(1.0f);
+        while (FaderScript.instance.doing) yield return null;
+
+        op.allowSceneActivation = true;
+    }
+}
